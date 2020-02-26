6bf540d667a2c682f700416cc4abbd338702898a
Tyler Kung <tkung12@gmail.com>
2020-02-25 18:11:16 -0800

    model_18 on cdn

--- src/Components/NewCity/Assets/index.js
+++ src/Components/NewCity/Assets/index.js
@@ -1,3 +1,3 @@
-// export const CityFile = 'https://sleepercdn.com/downloads/webtest/heropage_003.fbx';
+export const CityFile = 'https://sleepercdn.com/downloads/webtest/heropage_018.fbx';
 
-export const CityFile = './images/heropage_018.fbx';
+// export const CityFile = './images/heropage_018.fbx';
